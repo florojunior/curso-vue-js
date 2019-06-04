8923277fdccec97ccc538dd7292343d8ec9da1d0
florojunior <jrlima.reis@gmail.com>
2019-06-04 14:01:37 -0400

    Resolvendo desafio de componentização

---
+++ curso-vue/componente-desafio/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<v-app>
+		<v-navigation-drawer app>
+			<v-toolbar flat>
+				<v-list>
+					<v-list-tile>
+						<v-list-tile-title class="title">
+							Menu
+						</v-list-tile-title>
+					</v-list-tile>
+				</v-list>
+			</v-toolbar>
+
+			<v-divider></v-divider>
+
+			<v-list dense class="pt-0">
+				<v-list-tile v-for="item in itensMenu" :key="item.titulo">
+					<v-list-tile-action>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+    <base-top-bar/>
+    <base-content/>
+    <base-foot-bar/>
+	</v-app>
+</template>
+
+<script>
+
+  import Topbar from './Topbar.vue';
+  import Footbar from './FootBar.vue';
+  import Content from './Content.vue';
+
+  export default {
+    components:{'base-top-bar': Topbar, 'base-foot-bar': Footbar, 'base-content': Content},
+    data() {
+      return {
+        itensMenu: [
+          {titulo: 'Início', icon: 'dashboard'},
+          {titulo: 'Sobre', icon: 'question_answer'}
+        ]
+      }
+    }
+  }
+</script>
---
+++ curso-vue/componente-desafio/src/Topbar.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Desafio</span>
+				<span class="font-weight-light">Componente</span>
+			</v-toolbar-title>
+	</v-toolbar>
+</template>
+<script>
+	export default {
+		data(){
+		}
+	}
+</script>
+<style>
+</style>
